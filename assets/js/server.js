53e8aa707a6aca4188dcfe21ee02fc9a52ef5b69
Adam Lim <aedamjung@gmail.com>
2024-10-26 13:36:01 +0800

    Brush up resume and removed gallery

--- assets/js/server.js
+++ assets/js/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000; // You can change this to any port you prefer
+const PORT = 5001; // You can change this to any port you prefer
 
 // Middleware
 app.use(cors());
